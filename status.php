d0a9c78c9afb4aaa7a2c799596ee1dbae3a3d879
mixass <67761405+mixass@users.noreply.github.com>
2021-05-06 02:40:33 +0300

    Add files via upload

---
+++ status.php
@@ -0,0 +1,20 @@
+<?php
+$website = 'license.mixas.online';
+function the_ping_time($url = '', $port = 8080, $timeout = 180)
+{
+   if (!empty($url)) {
+       $request_time = microtime(true);
+       $socket = @fSockOpen($url, $port, $errno, $errstr, $timeout);
+       if (!$socket) {
+          return ('<h1> down xd </h1>');
+      } else {
+           $response_time = microtime(true);
+           return (round((($response_time - $request_time) * 1000), 0) . ' ms');
+       }
+   } else {
+       return (null);
+   }
+}
+
+
+echo(the_ping_time($website));
